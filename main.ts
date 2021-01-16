2f4b520c847b378a3421c32a78b88b5fe88a4b2e
francescoganci <76791249+francescoganci@users.noreply.github.com>
2021-01-16 15:40:33 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,50 +5,101 @@ input.onButtonPressed(Button.A, function () {
     while (true) {
         if (input.acceleration(Dimension.Strength) > 1500) {
             Caminhada += 1
-            basic.pause(65)
+        }
+        if (Caminhada <= 10) {
+            basic.showNumber(Caminhada / 2)
+            basic.showString("M")
+            basic.pause(2000)
+            basic.clearScreen()
+        } else if (Caminhada <= 100) {
+            basic.showNumber(Caminhada / 20)
+            basic.showString("Dam")
+            basic.pause(2000)
+            basic.clearScreen()
+        } else if (Caminhada <= 1000) {
+            basic.showNumber(Caminhada / 200)
+            basic.showString("Hm")
+            basic.pause(2000)
+            basic.clearScreen()
+        } else if (Caminhada <= 10000) {
+            basic.showNumber(Caminhada / 2000)
+            basic.showString("Km")
+            basic.pause(2000)
+            basic.clearScreen()
+        } else {
+            basic.showNumber(Caminhada / 2000)
+            basic.showString("Km")
+            basic.pause(2000)
+            basic.clearScreen()
+        }
+        if (Meta_diária == 10000) {
+            basic.showLeds(`
+                # # . # #
+                # # . # #
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+            basic.pause(100)
+            basic.clearScreen()
+            basic.showString("Bateu a sua meta diária")
+            basic.pause(100)
             basic.clearScreen()
         }
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    if (Caminhada <= 10) {
-        basic.showNumber(Caminhada / 2)
-        basic.showString("M")
-    } else if (Caminhada <= 100) {
-        basic.showNumber(Caminhada / 20)
-        basic.showString("Dam")
-    } else if (Caminhada <= 1000) {
-        basic.showNumber(Caminhada / 200)
-        basic.showString("Hm")
-    } else if (Caminhada <= 10000) {
-        basic.showNumber(Caminhada / 2000)
-        basic.showString("Km")
-    } else {
-        basic.showNumber(Caminhada / 2000)
-        basic.showString("Km")
-    }
-    if (Meta_diária == 10000) {
-        basic.showLeds(`
-            # # . # #
-            # # . # #
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-        basic.pause(100)
-        basic.clearScreen()
-        basic.showString("Bateu a sua meta diária")
-        basic.pause(100)
-        basic.clearScreen()
-    }
-    basic.pause(65)
-    basic.clearScreen()
-    basic.showNumber(Caminhada)
+	
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("Modo corrida ATIVADO")
     basic.pause(4000)
     basic.clearScreen()
+    while (true) {
+        if (input.acceleration(Dimension.Strength) > 1500) {
+            Corrida += 1
+        }
+        if (Corrida <= 10) {
+            basic.showNumber(Corrida / 1)
+            basic.showString("M")
+            basic.pause(2000)
+            basic.clearScreen()
+        } else if (Corrida <= 100) {
+            basic.showNumber(Corrida / 10)
+            basic.showString("Dam")
+            basic.pause(2000)
+            basic.clearScreen()
+        } else if (Corrida <= 1000) {
+            basic.showNumber(Corrida / 100)
+            basic.showString("Hm")
+            basic.pause(2000)
+            basic.clearScreen()
+        } else if (Corrida <= 10000) {
+            basic.showNumber(Corrida / 1000)
+            basic.showString("Km")
+            basic.pause(2000)
+            basic.clearScreen()
+        } else {
+            basic.showNumber(Corrida / 1000)
+            basic.showString("Km")
+            basic.pause(2000)
+            basic.clearScreen()
+        }
+        if (Meta_diária == 10000) {
+            basic.showLeds(`
+                # # . # #
+                # # . # #
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+            basic.pause(100)
+            basic.clearScreen()
+            basic.showString("Bateu a sua meta diária")
+            basic.pause(65)
+            basic.clearScreen()
+        }
+    }
 })
 let Corrida = 0
 let Meta_diária = 0
@@ -68,6 +119,9 @@ if (input.buttonIsPressed(Button.A)) {
     basic.pause(4000)
     basic.clearScreen()
 }
+basic.forever(function () {
+	
+})
 basic.forever(function () {
 	
 })
